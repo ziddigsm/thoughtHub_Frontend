459928ca0d7ae5d5f0c7358eeef0657fa6599dae
ziddigsm <158247382+ziddigsm@users.noreply.github.com>
2024-11-06 18:23:30 -0500

    added path redirection to login page if user hasnt logged in to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Settings } from "./components/Settings/settings";
 import Login from "./components/Login/login";
 import Home from "./components/Home/home";
 
 function App() {
+  let userData = localStorage.getItem("userData");
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/" element={userData ? <Home /> : <Login />} />
+        <Route path="/home" element={userData ? <Home /> : <Navigate to={"/"} />} />
+        <Route path="/settings" element={userData ? <Settings /> : <Navigate to={"/"} /> } />
       </Routes>
     </Router>
   );
